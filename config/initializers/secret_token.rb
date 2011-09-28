db44d19f034fcc366191770a4ce719fb2d5d8b59
slim <slim@dvgd-vcrweb-03.dvgd.oao.rzd>
2011-09-29 09:31:38 +1100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Funnypics::Application.config.secret_token = '9744bc898ea7b2016396939146d00b3777e542f841791842deb5118cc459143d5eee7cd51fe237b3379f184872ea3bb77e3be6d3a7f4696bd530c1b537a25ac1'
