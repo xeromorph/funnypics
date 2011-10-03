6c171018f2904e0fb6ab03a247b5d9edb706aa9f
Aleksey Sergeev <k30arenee@gmail.com>
2011-10-03 11:46:26 +1100

    image filenames made of hash

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,9 +1,11 @@
 class Image < ActiveRecord::Base
   attr_accessible :description, :img
+  attr_protected :img_size, :img_file_name, :img_content_type
   has_attached_file :img, :styles => { :thumb => ["150x150>", :jpg] },
-                          :url  => "/assets/images/:id/:style/:basename.:extension",
-                          #:url  => "/:hashed_path.:extension",
-                          :path => ":rails_root/public/assets/images/:id/:style/:basename.:extension"
+                          #:url  => "/assets/images/:id/:style/:basename.:extension",
+                          :hash_data => ":attachment",
+                          :url  => "/assets/:class/:style/:hash.:extension",
+                          :path => ":rails_root/public/assets/images/:style/:hash.:extension"
 
   validates_presence_of :description
   validates_attachment_presence :img
