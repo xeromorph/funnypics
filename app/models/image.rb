cf7c6c900dedcc20861a8c6e86876d754a88ef43
Aleksey Sergeev <k30arenee@gmail.com>
2011-10-04 15:12:49 +1100

    fix hash_data and accepted mime types

--- app/models/image.rb
+++ app/models/image.rb
@@ -3,12 +3,12 @@ class Image < ActiveRecord::Base
   attr_protected :img_size, :img_file_name, :img_content_type
   has_attached_file :img, :styles => { :thumb => ["150x150>", :jpg] },
                           #:url  => "/assets/images/:id/:style/:basename.:extension",
-                          :hash_data => ":attachment",
+                          :hash_data => ":class/:attachment/:id",
                           :url  => "/assets/:class/:style/:hash.:extension",
                           :path => ":rails_root/public/assets/images/:style/:hash.:extension"
 
   validates_presence_of :description
   validates_attachment_presence :img
   validates_attachment_size :img, :less_than => 10.megabytes
-  validates_attachment_content_type :img, :content_type => ['image/jpeg', 'image/png', 'image/gif']
+  validates_attachment_content_type :img, :content_type => ['image/jpeg', 'image/pjpeg', 'image/jpg', 'image/png', 'image/x-png', 'image/gif']
 end
