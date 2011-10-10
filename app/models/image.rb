27d079745174c98447bbd185e944adedfce11085
Aleksey Sergeev <k30arenee@gmail.com>
2011-10-10 15:05:44 +1100

    next|prev item method added

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,5 +1,9 @@
 class Image < ActiveRecord::Base
   attr_accessible :description, :img
+  has_many :taggings, :dependent => :destroy
+  has_many :tags, :through => :taggings
+  attr_writer :tag_names
+  before_save :assign_tags
   attr_protected :img_size, :img_file_name, :img_content_type
   has_attached_file :img, :styles => { :thumb => ["150x150>", :jpg] },
                           #:url  => "/assets/images/:id/:style/:basename.:extension",
@@ -11,4 +15,25 @@ class Image < ActiveRecord::Base
   validates_attachment_presence :img
   validates_attachment_size :img, :less_than => 10.megabytes
   validates_attachment_content_type :img, :content_type => ['image/jpeg', 'image/pjpeg', 'image/jpg', 'image/png', 'image/x-png', 'image/gif']
+  
+  def tag_names
+    @tag_names || tags.map(&:name).join(', ')
+  end
+
+  def next
+    Image.first(:conditions => ['id > ?', self.id], :order => 'id ASC') || Image.first
+  end
+  
+  def prev
+    Image.last(:conditions => ['id < ?', self.id], :order => 'id ASC') || Image.last
+  end
+
+  private
+  def assign_tags
+    if @tag_names
+      self.tags = @tag_names.split(/,\s*/).map do |tname|
+        Tag.find_or_initialize_by_name(tname)
+      end
+    end
+  end
 end
