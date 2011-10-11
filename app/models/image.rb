93bb0c2c87304dee29718b7a7450b6d75b795b15
Aleksey Sergeev <k30arenee@gmail.com>
2011-10-11 12:01:35 +1100

    lil cleanup

--- app/models/image.rb
+++ app/models/image.rb
@@ -3,7 +3,8 @@ class Image < ActiveRecord::Base
   has_many :taggings, :dependent => :destroy
   has_many :tags, :through => :taggings
   attr_writer :tag_names
-  before_save :assign_tags
+  #before_save :assign_tags
+  before_validation :assign_tags
   attr_protected :img_size, :img_file_name, :img_content_type
   has_attached_file :img, :styles => { :thumb => ["150x150>", :jpg] },
                           #:url  => "/assets/images/:id/:style/:basename.:extension",
@@ -12,6 +13,7 @@ class Image < ActiveRecord::Base
                           :path => ":rails_root/public/assets/images/:style/:hash.:extension"
 
   validates_presence_of :description
+  validates_presence_of :tags
   validates_attachment_presence :img
   validates_attachment_size :img, :less_than => 10.megabytes
   validates_attachment_content_type :img, :content_type => ['image/jpeg', 'image/pjpeg', 'image/jpg', 'image/png', 'image/x-png', 'image/gif']
@@ -30,10 +32,9 @@ class Image < ActiveRecord::Base
 
   private
   def assign_tags
-    if @tag_names
-      self.tags = @tag_names.strip.downcase.split(/\s*,\s*/).map do |tname|
-        Tag.find_or_initialize_by_name(tname) if tname.present?
-      end
+    return if @tag_names.blank? 
+    self.tags = @tag_names.strip.downcase.split(/\s*,\s*/).map do |tname|
+      Tag.find_or_initialize_by_name(tname) if tname.present?
     end
   end
 end
